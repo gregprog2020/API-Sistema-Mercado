dc0cafba6f349649a5da1561b017520aac3db40e
Anderson Batista <batista.anderson92@gmail.com>
2024-06-14 12:04:44 -0300

    Adicionar arquivos de projeto.

---
+++ WebApplication1/Data/ApiDbContext.cs
@@ -0,0 +1,14 @@
+﻿using API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class ApiDbContext : DbContext
+    {
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options)
+        {
+        }
+            public DbSet<Produto> Produtos { get; set; }
+        
+    }
+}
---
+++ WebApplication1/Models/Produto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Models
+{
+    public class Produto
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [Range(1, int.MaxValue, ErrorMessage = "O valor deve ser maior que zero")]
+        public decimal Preco { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public int Quantidade { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public string Descricao { get; set; }
+    }
+}
